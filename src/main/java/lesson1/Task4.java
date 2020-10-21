477cdbc450ee501f9189e12e30950e99b431ac6f
vitalii_radchenko <vetalp89@gmail.com>
2020-10-21 13:16:49 +0300

    4 task done!

--- src/main/java/lesson1/Task4.java
+++ src/main/java/lesson1/Task4.java
@@ -9,21 +9,19 @@ System.out.println, System.out.print можно использовать тол�
 public class Task4 {
 
     public static void main(String[] args) {
-        String s = "Vladimir";
-        System.out.println(
-                s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                        + s);
-        System.out.println(
-                s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                        + s);
-        System.out.println(
-                s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                        + s);
-        System.out.println(
-                s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                        + s);
-        System.out.println(
-                s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                        + s);
+        String name = "Vitalii";
+        int s = 0;
+        do {
+            System.out.println();
+            s++;
+
+            int c = 0;
+            do {
+                System.out.print(name + " ");
+                c++;
+            }
+            while (c < 10);
+        }
+        while (s < 5);
     }
 }
