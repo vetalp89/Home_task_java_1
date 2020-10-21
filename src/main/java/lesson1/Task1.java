f08c44d6e5bcaf7e9cad8be51608c3dd886f890c
vitalii_radchenko <vetalp89@gmail.com>
2020-10-21 12:23:10 +0300

    1 task done!

--- src/main/java/lesson1/Task1.java
+++ src/main/java/lesson1/Task1.java
@@ -4,11 +4,20 @@ public class Task1 {
     /* Минимум трех чисел
     Написать функцию, которая вычисляет минимум из трёх чисел.
     */
-    public static void main(String[] args) {
-        //System.out.println(min(5,2,3));
-    }
 
     public static int min(int a, int b, int c) {
-        return 0;
+        if (a < b && a < c) {
+            return a;
+        } else if (b < a && b < c) {
+            return b;
+        } else {
+            return c;
+        }
     }
+
+    public static void main(String[] args) {
+        System.out.println("Minimal number: " + min(5,2,3));
+    }
+
+
 }
