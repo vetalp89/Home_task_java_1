07c56af2ed204f45f87581911c0a93e2e57d45a3
vitalii_radchenko <48300322+vetalp89@users.noreply.github.com>
2020-10-29 19:22:45 +0200

    Update Task5.java

--- src/main/java/lesson1/Task5.java
+++ src/main/java/lesson1/Task5.java
@@ -1,18 +1,29 @@
 package lesson1;
 
-/**
- * Реализовать функцию, которая принимает строку и возвращает ее же в обратном виде
- * например "Hello world!!!" -> "!!!dlrow olleH"
- */
+// Вхідне слово віддзеркалюємо. Розбиваємо вхідне та вихідне слова на массиви.
+ 
+ 
 public class Task5 {
 
-    public static void main(String[] args) {
-        String name = "Hello World";
+   public static void main(String[] args) {
+        String name = "ABCDE";
         int leng = name.length();
         String rev = "";
         for (int i = leng - 1; i >= 0; i--) {
             rev = rev + name.charAt(i);
         }
         System.out.println("Revers of: " + name + " is: " + rev);
+
+
+        char[] result = name.toCharArray();
+        System.out.println("Char array: ");
+        for (int i = 0; i < result.length; i++)
+            System.out.println("Element [" + i + "]: " + result[i]);
+
+
+        char[] result2 = rev.toCharArray();
+        System.out.println("Reverse: ");
+        for (int j = 0; j < result2.length; j++)
+            System.out.println("Element [" + j + "]: " + result2[j]);
     }
 }
